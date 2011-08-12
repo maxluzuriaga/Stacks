9f505f30725daaec9ecdebb2b77de0f1bdc8cc70
Max Luzuriaga <max@luzuriaga.com>
2011-08-12 16:46:35 -0400

    Added files for STEmptyDataSetView.

---
+++ Stacks/Classes/STEmptyDataSetView.m
@@ -0,0 +1,31 @@
+//
+//  STEmptyDataSetView.m
+//  Stacks
+//
+//  Created by Max Luzuriaga on 8/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "STEmptyDataSetView.h"
+
+@implementation STEmptyDataSetView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
