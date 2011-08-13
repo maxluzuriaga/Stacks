00488a50c2d98c0b0fee6425ade81b9ea34be4cc
Max Luzuriaga <max@luzuriaga.com>
2011-08-13 17:26:49 -0400

    Deleted DetailViewController, created STStackDetailViewController

---
+++ Stacks/Classes/STStackDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  STStackDetailViewController.h
+//  Stacks
+//
+//  Created by Max Luzuriaga on 8/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STStackDetailViewController : UITableViewController
+
+@end
