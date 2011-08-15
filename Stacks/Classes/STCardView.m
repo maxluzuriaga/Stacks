fc966652cdcf42dd2b507dc901656b46b52929ab
Max Luzuriaga <max@luzuriaga.com>
2011-08-15 15:00:25 -0400

    Added file for STCardView.

---
+++ Stacks/Classes/STCardView.m
@@ -0,0 +1,31 @@
+//
+//  STCardView.m
+//  Stacks
+//
+//  Created by Max Luzuriaga on 8/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "STCardView.h"
+
+@implementation STCardView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
