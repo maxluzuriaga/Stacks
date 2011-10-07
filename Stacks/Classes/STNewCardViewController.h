94118fb51420b21acd56f309964ddfc68faa8d52
Max Luzuriaga <max@luzuriaga.com>
2011-10-07 12:33:03 -0400

    Added files for STNewCardViewController.

---
+++ Stacks/Classes/STNewCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  STNewCardViewController.h
+//  Stacks
+//
+//  Created by Max Luzuriaga on 10/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STNewCardViewController : UIViewController
+
+@end
